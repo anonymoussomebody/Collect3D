2d834304458614906c697deec98484385a947fc0
Prouser123 <jcahill773@gmail.com>
2017-11-02 21:15:04 +0000

    Code cleanup

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,26 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
-    // Define Private Variables
+    // Variables
 
     // Level Settings
     private Rigidbody rb;
-    private int count;
     private string levelNumber;
-    private bool isPaused = false;
-
-    // Define Public Variables
-
-    // Level GUI/Settings
     public float speed;
-    public Text countText;
     public Text levelText;
     public GameObject levelInfo;
-    // Level Pause/End
-    public GameObject levelCompleted;
-    public GameObject levelPaused;
 
-    // NEW Mouse
+    // Mouse Controls (Disabled)
     Vector2 mLook;
     Vector2 mSmoothV;
     public float mSensitivity = 5.0F;
@@ -36,39 +26,16 @@ public class PlayerController : MonoBehaviour {
     {
         //Cursor.lockState = CursorLockMode.Locked;
         rb = GetComponent<Rigidbody>();
-        count = 0;
-        LevelCompleted();
-        levelCompleted.SetActive(false);
-        levelPaused.SetActive(false);
         LevelInfo();
     }
 
     void FixedUpdate()
     {
+        // Rigidbody Movement
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
-
         rb.AddForce(movement);
-
-        // Pause Menu
-        //KeyCode pause = KeyCode.Escape;
-        //if (isPaused == false)
-        //{
-        //    if (Input.GetKeyDown(pause))
-        //    {
-        //        levelPaused.SetActive(true);
-        //        isPaused = true;
-        //    }
-        //} else if (isPaused == true)
-        //{
-        //    if (Input.GetKeyDown(pause))
-        //    {
-        //        levelPaused.SetActive(false);
-        //        isPaused = false;
-        //    }
-        //}
 
         // Escape Key for Menu Exit
         if (Input.GetKeyDown("escape"))
@@ -77,7 +44,7 @@ public class PlayerController : MonoBehaviour {
             SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
         }
 
-        // NEW Mouse Input
+        // Mouse Input (Disabled)
         var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
         md = Vector2.Scale(md, new Vector2(mSensitivity * mSmoothing, mSensitivity * mSmoothing));
         mSmoothV.x = Mathf.Lerp(mSmoothV.x, md.x, 1f / mSmoothing);
@@ -89,97 +56,15 @@ public class PlayerController : MonoBehaviour {
         //playerHost.transform.localRotation = Quaternion.AngleAxis(mLook.x, playerHost.transform.up);
     }
 
-    public void ReturnGameClicked()
-    {
-        levelPaused.SetActive(false);
-        isPaused = false;
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        // Destroy(other.gameObject);
-        if (other.gameObject.CompareTag("PickUp"))
-        {
-            other.gameObject.SetActive(false);
-            count = count + 1;
-            LevelCompleted();
-        }
-    }
-
-    void LevelCompleted()
-    {
-        countText.text = "Count: " + count.ToString();
-
-        // Level 1 - 4 PickUps
-        if (levelNumber == "L1")
-        {
-            if (count == 4)
-            {
-                levelCompleted.SetActive(true);
-
-                // Save string when level is completed
-                if (PlayerPrefs.GetString("HLC") == "")
-                {
-                    PlayerPrefs.SetString("HLC", "Level 1");
-                    Debug.Log("Set HLC to 1.");
-                }
-            }
-        }
-
-        // Level 2 - 4 PickUps
-        else if (levelNumber == "L2")
-        {
-            if (count == 4)
-            {
-                levelCompleted.SetActive(true);
-
-                // Save string when level is completed
-                if (PlayerPrefs.GetString("HLC") == "Level 1")
-                {
-                    PlayerPrefs.SetString("HLC", "Level 2");
-                    Debug.Log("Set HLC to 2.");
-                }
-            }
-        }
-
-        // Level 3 - 8 PickUps
-        else if (levelNumber == "L3")
-        {
-            if (count == 8)
-            {
-                levelCompleted.SetActive(true);
-
-                // Save string when level is completed
-                if (PlayerPrefs.GetString("HLC") == "Level 2")
-                {
-                    PlayerPrefs.SetString("HLC", "Level 3");
-                    Debug.Log("Set HLC to 3.");
-                }
-            }
-        }
-    }
-
     void LevelInfo()
     {
         levelNumber = levelInfo.transform.name;
         Debug.Log("levelNumber: " + levelNumber);
 
-        // Level 1
-        if (levelNumber == "L1")
+        // Tutorial v2
+        if (levelNumber == "level.Tutorial-v2")
         {
-            levelText.text = "Level 1";
-        }
-
-        // Level 2
-        else if (levelNumber == "L2")
-        {
-            levelText.text = "Level 2";
-        }
-
-        // Level 3
-        else if (levelNumber == "L3")
-        {
-            levelText.text = "Level 3";
+            levelText.text = "Level: Tutorial v2";
         }
         else
         {
